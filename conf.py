fbfb06ea5b9823f8753b6f5e4c73059013877f40
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-09-20 09:50:34 -0700

    build blog outline

--- conf.py
+++ conf.py
@@ -493,7 +493,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "astrohackweek"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
